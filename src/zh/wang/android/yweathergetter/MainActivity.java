f9f503a88ce12b690f029209e3ead3948d237905
viennakanon <viennakanon@viennakanon-Inspiron-N5110.(none)>
2013-03-23 17:07:31 +0900

    Change instructions in README

--- src/zh/wang/android/yweathergetter/MainActivity.java
+++ src/zh/wang/android/yweathergetter/MainActivity.java
@@ -1,12 +1,13 @@
 package zh.wang.android.yweathergetter;
 
 import zh.wang.android.utils.YahooWeather4a.WeatherInfo;
+import zh.wang.android.utils.YahooWeather4a.YahooWeatherInfoListener;
 import zh.wang.android.utils.YahooWeather4a.YahooWeatherUtils;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements YahooWeatherInfoListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,8 +15,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         YahooWeatherUtils yahooWeatherUtils = YahooWeatherUtils.getInstance();
-        WeatherInfo weatherInfo = yahooWeatherUtils.queryYahooWeather(getApplicationContext(), "Tokyo");
+        yahooWeatherUtils.queryYahooWeather(getApplicationContext(), "Tokyo", this);
+    }
 
+	@Override
+	public void gotWeatherInfo(WeatherInfo weatherInfo) {
+		// TODO Auto-generated method stub
         if(weatherInfo != null) {
         	TextView tv = (TextView) findViewById(R.id.textview_weather_info);
 			tv.setText(weatherInfo.getTitle() + "\n"
@@ -25,7 +30,6 @@ public class MainActivity extends Activity {
 					+ "forecast weather: " + weatherInfo.getForecast1Text() + "\n" 
 					+ "forecast weather: " + weatherInfo.getForecast2Text());
         } 
-      
-    }
+	}
 
 }
