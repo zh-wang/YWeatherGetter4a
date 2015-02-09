99f5305bf8f6fca8a6586baf1a5034cb0cd8b259
zh-wang <viennakanon@gmail.com>
2015-02-09 16:27:54 +0900

    add test project

---
+++ sample_app-test/src/zh/wang/android/yweathergetter/test/MainActivityTest.java
@@ -0,0 +1,13 @@
+package zh.wang.android.yweathergetter.test;
+
+import zh.wang.android.yweathergetter.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+        // TODO Auto-generated constructor stub
+    }
+
+}
